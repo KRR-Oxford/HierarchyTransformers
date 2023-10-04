5eedcd567c1efe12667a0e674c0d8fe438fa3d5f
Lawhy <yuanhe.cs.ai@gmail.com>
2023-10-04 03:42:33 +0100

    update

--- holm/taxonomy.py
+++ holm/taxonomy.py
@@ -1,27 +1,11 @@
-# Copyright 2023 Yuan He. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from __future__ import annotations
 
-from typing import Optional, Tuple, List, Dict, Union
-import pandas as pd
-import networkx as nx
-import numpy as np
-import torch
 from yacs.config import CfgNode
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
 from sklearn.model_selection import train_test_split
-from deeponto.onto import Taxonomy, OntologyTaxonomy, WordnetTaxonomy, TaxonomyNegativeSampler
+from deeponto.onto import Taxonomy, TaxonomyNegativeSampler
 
 
 def transitivity_data_splitting(taxonomy: Taxonomy):
@@ -34,3 +18,37 @@ def transitivity_data_splitting(taxonomy: Taxonomy):
     train_edges, eval_edges = train_test_split(transitive_edges, test_size=0.2)
     val_edges, test_edges = train_test_split(eval_edges, test_size=0.5)
     return CfgNode({"base": base_edges, "train": train_edges, "val": val_edges, "test": test_edges})
+
+
+def subsumption_data_splitting(taxonomy: Taxonomy):
+    # the subsumption setting sets a portion of edges as "missing"
+    neg_sampler = TaxonomyNegativeSampler(taxonomy)
+    base_edges = taxonomy.edges
+    train_edges, eval_edges = train_test_split(base_edges, test_size=0.2)
+    val_edges, test_edges = train_test_split(eval_edges, test_size=0.5)
+
+    train_df = pd.DataFrame(train_edges, columns=["SrcEntity", "TgtEntity"])
+
+    val_df = []
+    for s, t in tqdm(val_edges, desc="Validation Set Negative Sampling"):
+        t_cands = []
+        while len(t_cands) < 100:
+            t_cands += neg_sampler.sample(s, 200)
+            t_cands = list(set(t_cands))
+        t_cands = [t] + t_cands[:99]  # 1 correct candidate and 99 negative candidates
+        np.random.shuffle(t_cands)
+        val_df.append((s, t, t_cands))
+    val_df = pd.DataFrame(val_df, columns=["SrcEntity", "TgtEntity", "TgtCandidates"])
+
+    test_df = []
+    for s, t in tqdm(test_edges, desc="Test Set Negative Sampling"):
+        t_cands = []
+        while len(t_cands) < 100:
+            t_cands += neg_sampler.sample(s, 200)
+            t_cands = list(set(t_cands))
+        t_cands = [t] + t_cands[:99]  # 1 correct candidate and 99 negative candidates
+        np.random.shuffle(t_cands)
+        test_df.append((s, t, t_cands))
+    test_df = pd.DataFrame(test_df, columns=["SrcEntity", "TgtEntity", "TgtCandidates"])
+
+    return train_df, val_df, test_df
