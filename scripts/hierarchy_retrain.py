fbcda39de4ba4ae92486419a705c469ff9c7a306
Lawhy <yuanhe.cs.ai@gmail.com>
2024-12-03 01:13:36 +0000

    update

--- scripts/hierarchy_retrain.py
+++ scripts/hierarchy_retrain.py
@@ -13,16 +13,17 @@
 # limitations under the License.
 
 from deeponto.utils import load_file, set_seed
-from torch.utils.data import DataLoader
 import logging
 import os
 import click
 from yacs.config import CfgNode
+from sentence_transformers.trainer import SentenceTransformerTrainer
+from sentence_transformers.training_args import SentenceTransformerTrainingArguments
 
-from hierarchy_transformers.models import *
-from hierarchy_transformers.losses import *
+from hierarchy_transformers.models import HierarchyTransformer
+from hierarchy_transformers.losses import HierarchyTransformerLoss
 from hierarchy_transformers.evaluation import HierarchyTransformerEvaluator
-from hierarchy_transformers.models.utils import prepare_hierarchy_examples, load_hierarchy_dataset, get_torch_device
+from hierarchy_transformers.datasets import load_hf_dataset
 
 
 logger = logging.getLogger(__name__)
@@ -31,65 +32,74 @@ logger.setLevel(logging.INFO)
 
 @click.command()
 @click.option("-c", "--config_file", type=click.Path(exists=True))
-@click.option("-g", "--gpu_id", type=int, default=0)
-def main(config_file: str, gpu_id: int):
+def main(config_file: str):
     set_seed(8888)
     config = CfgNode(load_file(config_file))
 
-    # load dataset
-    data_path = config.data_path
-    dataset, entity_lexicon = load_hierarchy_dataset(data_path)
+    # 1. Load dataset and pre-trained model
+    dataset = load_hf_dataset(config.dataset_path, config.dataset_name)
+    model = HierarchyTransformer.from_pretrained(model_name_or_path=config.model_path)
 
-    train_examples = prepare_hierarchy_examples(
-        entity_lexicon, dataset["train"], config.apply_hard_negatives, config.apply_triplet_loss
-    )
-    train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=config.train_batch_size)
-
-    val_examples = prepare_hierarchy_examples(entity_lexicon, dataset["val"], config.apply_hard_negatives)
-    test_examples = prepare_hierarchy_examples(entity_lexicon, dataset["test"], config.apply_hard_negatives)
-
-    # load pre-trained model
-    device = get_torch_device(gpu_id)
-    model = HierarchyTransformer.from_pretrained(model_name_or_path=config.pretrained, device=device)
-
-    # loss
-    losses = []
-
-    if config.loss.cluster.weight > 0.0:
-        if config.apply_triplet_loss:
-            cluster_loss = HyperbolicClusteringLoss(model.manifold, config.loss.cluster.margin)
-        else:
-            cluster_loss = ClusteringConstrastiveLoss(
-                model.manifold, config.loss.cluster.positive_margin, config.loss.cluster.margin
-            )
-        losses.append((config.loss.cluster.weight, cluster_loss))
-
-    if config.loss.centri.weight > 0.0:
-        centri_loss_class = HyperbolicCentripetalLoss if config.apply_triplet_loss else CentripetalContrastiveLoss
-        centri_loss = centri_loss_class(model.manifold, model.embed_dim, config.loss.centri.margin)
-        losses.append((config.loss.centri.weight, centri_loss))
-
-    hyper_loss = HyperbolicLoss(model, config.apply_triplet_loss, *losses)
-    print(hyper_loss.get_config_dict())
-    hyper_loss.to(device)
-    hit_evaluator = HierarchyTransformerEvaluator(
-        device=device,
-        eval_batch_size=config.eval_batch_size,
-        val_examples=val_examples,
-        test_examples=test_examples,
-        train_examples=train_examples if config.eval_train else None,
+    # 2. set up the loss function
+    hit_loss = HierarchyTransformerLoss(
+        model=model,
+        clustering_loss_weight=config.hit_loss.clustering_loss_weight,
+        clustering_loss_margin=config.hit_loss.clustering_loss_margin,
+        centripetal_loss_weight=config.hit_loss.centripetal_loss_weight,
+        centripetal_loss_margin=config.hit_loss.centripetal_loss_margin,
     )
 
-    data_suffix = config.data_path.split(os.path.sep)[-1]
-    model.fit(
-        train_objectives=[(train_dataloader, hyper_loss)],
-        epochs=config.num_epochs,
-        optimizer_params={"lr": float(config.learning_rate)},  # defaults to 2e-5
-        # steps_per_epoch=20,  # for testing use
+    print(hit_loss.get_config_dict())
+
+    # 3. Define a validation evaluator for use during training.
+    val_evaluator = HierarchyTransformerEvaluator(
+        child_entities=dataset["val"]["child"],
+        parent_entities=dataset["val"]["parent"],
+        labels=dataset["val"]["label"],
+        batch_size=config.eval_batch_size,
+    )
+
+    # 4. Define the training arguments
+    dataset_suffix = config.data_path.split(os.path.sep)[-1]
+    output_dir = f"experiments/HiT-{dataset_suffix}-{config.dataset_name}"
+    args = SentenceTransformerTrainingArguments(
+        output_dir=output_dir,
+        num_train_epochs=config.num_train_epochs,
+        learning_rate=config.learning_rate,
+        per_device_train_batch_size=config.train_batch_size,
+        per_device_eval_batch_size=config.eval_batch_size,
+        warmup_ratio=0.1,
         warmup_steps=config.warmup_steps,
-        evaluator=hit_evaluator,
-        output_path=f"experiments/HiT-{config.pretrained}-{data_suffix}-hard={config.apply_hard_negatives}-triplet={config.apply_triplet_loss}-cluster={list(config.loss.cluster.values())}-centri={list(config.loss.centri.values())}",
+        eval_strategy="epochs",
+        save_strategy="epochs",
+        save_total_limit=2,
+        logging_steps=100,
+        load_best_model_at_end=True
     )
+
+    # 5. Create the trainer & start training
+    trainer = SentenceTransformerTrainer(
+        model=model,
+        args=args,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["val"],
+        loss=hit_loss,
+        evaluator=val_evaluator,
+    )
+    trainer.train()
+
+    # 6. Evaluate the model performance on the STS Benchmark test dataset
+    test_evaluator = HierarchyTransformerEvaluator(
+        child_entities=dataset["test"]["child"],
+        parent_entities=dataset["test"]["parent"],
+        labels=dataset["test"]["label"],
+        batch_size=config.eval_batch_size,
+    )
+    test_evaluator(model)
+
+    # 7. Save the trained & evaluated model locally
+    final_output_dir = f"{output_dir}/final"
+    model.save(final_output_dir)
 
 
 if __name__ == "__main__":
--- src/hierarchy_transformers/evaluation/__init__.py
+++ src/hierarchy_transformers/evaluation/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .hit_eval import HierarchyEvaluator
+from .hit_eval import HierarchyTransformerEvaluator
