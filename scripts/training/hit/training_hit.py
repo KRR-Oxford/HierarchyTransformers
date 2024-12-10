971cc9ab1b440bd6608ea21fc666ff7705bfb4e3
Lawhy <yuanhe.cs.ai@gmail.com>
2024-12-10 13:35:52 +0000

    [chore] refactor hit training script

--- scripts/training/hit/training_hit.py
+++ scripts/training/hit/training_hit.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""This training script is hierarchy re-training of HiT models."""
 
 from deeponto.utils import set_seed, create_path, load_file, save_file
 import os, sys, logging, click
